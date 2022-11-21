3114899f86a31e1b641d8e1aad93bb4d0646e8ba
a.gazalov <gazalov@list.ru>
2022-11-21 17:20:25 +0300

    Task about doggy

---
+++ doggy/Program.cs
@@ -0,0 +1,22 @@
+﻿float count = 0, distance = 10000, firstFriendSpeed = 1, secondFriendSpeed = 2, dogSpeed = 5, friend = 2;
+float time;
+//count = 0; distance = 10000; firstFriendSpeed = 1; secondFriendSpeed = 2; dogSpeed = 5; friend = 2
+
+while(distance > 10)
+    {
+    if (friend == 1)
+        {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        }
+    else
+        {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        }
+    count = count + 1;        
+    }
+Console.WriteLine(count);
+
